bd8cffdddc1b16f3e8bfe43ccafbc3c711d3eb6f
Code <code@macbook-air-de-theatre.home>
2019-07-03 17:40:09 +0200

    first commit

---
+++ src/gpio_access.rs
@@ -0,0 +1,7 @@
+use std::time::Duration;
+
+mod gpio_access {
+    pub fn togglePin(pin: u8, time: Option<std::time::Duration>){
+        println!("Test");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#[macro_use]
+extern crate json;
+
+
+use std::net::TcpStream;
+use std::io::Write;
+use std::io;
+use std::io::Read;
+use std::fs;
+mod gpio_access;
+
+fn main() {
+
+    let data = fs::read_to_string("save.txt").expect("could not read file");
+    let data: Vec<&str> = data.split('\n').collect();
+    let auth = object!{
+        "type" => "header",
+        "headerType" => "authentificate",
+        "id" => data[1]
+    };
+
+
+    let mut stream = TcpStream::connect(data[0]).expect("could not connect to server");
+
+    stream.write((auth.dump() + "␄").as_bytes()).expect("could not send to server");
+        
+    stream.set_nonblocking(true).expect("could not set socket to non blocking");
+    let mut message = "".to_owned();  
+    loop{
+        let mut buffer = vec![0; 2048];
+        match stream.read(&mut buffer){
+            Ok(_) => {
+                message.push_str(std::str::from_utf8(&buffer).expect("could not convert to string"));
+                if message.find('␄') != None{
+                    break;
+                }
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            },
+            Err(ref e) => panic!("encountered io error : {}", e)
+        };
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+
+    let messages: Vec<&str> = message.split('␄').collect();
+    for message in messages {
+        print!("nouveau message : {}", message);
+        let obj = json::parse(message).expect("invalid message");
+        match obj["type"].as_str().expect("no message type found") {
+            "authentificated" => {
+                fs::write("save.txt", std::format!("{}\n{}",data[0], obj["id"].as_str().expect("no valid id provided")))
+                .expect("writing to file failed");
+                
+            },
+
+
+
+            _=>panic!("Uncovered message type")
+        }
+    }
+
+}
